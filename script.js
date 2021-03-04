4a89f3f909e6429185797dd024e7acc874443031
Eric Treadway <eric.s.treadway@gmail.com>
2021-03-03 18:21:28 -0600

    some work was added on checking for wins in col

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
+var boardElement = document.getElementById('board');
+var board = [];
+var turn = 'x';
+// this iterates through every row
+for (var row = 0; row < 3; row++) {
+    if (board[row] == undefined) {
+        board[row] = [];
+    }
 
-    var boardElement = document.getElementById('board');
-    var board = [];
-    var turn = 'x';
-    //col
-    for (var row = 0; row < 3; row++) {
-        if (board[row] == undefined) {
-            board[row] = [];
+    // this iterates through every col 
+    for (var col = 0; col < 3; col++) {
+        //this 
+        if (col == 0){
+            boardElement.appendChild(document.createElement('br'));
         }
 
-        
-        for (var col = 0; col < 3; col++) {
-            if (col == 0){
-                boardElement.appendChild(document.createElement('br'));
-            }
+        var slot = document.createElement("img");
+        slot.className = "blank";
+        slot.src = 'images/blank.png';
 
-            var slot = document.createElement("img");
-            slot.className = "blank";
-            slot.src = 'images/blank.png';
-
-            slot.addEventListener("click", function () {
-                //This if statement makes sure the spot is not taken
-                if (this.className == 'blank'){
-                    //This checks who's turn it is
-                    if (turn == 'x'){
-                        this.src = 'images/x.png';
-                        this.className += ' x';
-                        turn = 'o';
-                    } else {
-                        this.src = 'images/o.png';
-                        this.className += ' o';
-                        turn = 'x';
-                    }
-                    checkForWinOnRow()
+        slot.addEventListener("click", function () {
+            //This if statement makes sure the spot is not taken
+            if (this.className == 'blank'){
+                //This checks who's turn it is
+                if (turn == 'x'){
+                    this.src = 'images/x.png';
+                    this.className += ' x';
+                    turn = 'o';
+                } else {
+                    this.src = 'images/o.png';
+                    this.className += ' o';
+                    turn = 'x';
                 }
-                
-            });
-            board[row][col] = slot;
-            boardElement.appendChild(slot);
-        }//end of j
-        
-    }//end of i
-    console.log(board);
+                rowCheckForWin()
+            }
+            
+        });
+        board[row][col] = slot;
+        boardElement.appendChild(slot);
+    }//end of j
     
-    function checkForWinOnRow() {
-        for (var i=0; i<3; i++){
+}//end of i
+
+//checks for a win on each row
+function rowCheckForWin() {
+    for (var i=0; i<3; i++){
         var checkedRow = board[i];
 
         //checks if row matches
         if (checkedRow[0].className==checkedRow[1].className 
         && checkedRow[1].className==checkedRow[2].className){
+            //checks for a win in each row
             for (var j = 0; j<3; j++) {
                 //This if makes sure the space isn't empty
                     if (checkedRow[j].className != 'blank'){
@@ -57,21 +58,17 @@
                     
                 }
             }
-        }
     }
+}
 
 
+function colCheckForWin() {
+    for (var y=0; y<3; y++){
+        var checkedCol = []
+        for (var x=0; x<3; x++){
+            checkedCol.push(board[x][0])
+        }
+        console.log(checkedCol)
+    }  
+}
     
-
-    // make these images, grab 3 images from the web-- x, o, and blank
-    /*
-    board[0][0].innerHTML = 'none'
-    board[0][1].innerHTML = 'none'
-    board[0][2].innerHTML = 'none'
-    board[1][0].innerHTML = 'none'
-    board[1][1].innerHTML = 'none'
-    board[1][2].innerHTML = 'none'
-    board[2][0].innerHTML = 'none'
-    board[2][1].innerHTML = 'none'
-    board[2][2].innerHTML = 'none'
-    */
