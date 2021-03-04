712f4731356b8b995bcb7abff8fcb56adaa1086d
Eric Treadway <eric.s.treadway@gmail.com>
2021-03-04 09:43:54 -0600

    cleaned up the commints a bit and made the winner class more consistent with other classes

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 var boardElement = document.getElementById('board');
 var board = [];
 var turn = 'x';
-// this iterates through every row
+// iterates through every row
 for (var row = 0; row < 3; row++) {
     if (board[row] == undefined) {
         board[row] = [];
     }
 
-    // this iterates through every col 
+    // iterates through every col 
     for (var col = 0; col < 3; col++) {
-        //this 
+        // this makes each row appear on a new line
         if (col == 0){
             boardElement.appendChild(document.createElement('br'));
         }
 
+        // creates each slot and gives it a blank image
         var slot = document.createElement("img");
         slot.className = "blank";
         slot.src = 'images/blank.png';
 
+        // gives each slot click functionality
         slot.addEventListener("click", function () {
-            //This if statement makes sure the spot is not taken
+            // if statement makes sure the spot is not taken
             if (this.className == 'blank'){
-                //This checks who's turn it is
+                // checks who's turn it is and assigns proper image
                 if (turn == 'x'){
                     this.src = 'images/x.png';
-                    this.className += ' x';
+                    this.className = 'x';
                     turn = 'o';
                 } else {
                     this.src = 'images/o.png';
-                    this.className += ' o';
+                    this.className = 'o';
                     turn = 'x';
                 }
                 rowCheckForWin();
@@ -38,31 +40,31 @@ for (var row = 0; row < 3; row++) {
         });
         board[row][col] = slot;
         boardElement.appendChild(slot);
-    }//end of j
+    }//end of col
     
-}//end of i
+}//end of row
 
-//checks for a win on each row
+// checks for a win on each row
 function rowCheckForWin() {
     for (var i=0; i<3; i++){
         var checkedRow = board[i];
 
-        //checks if row matches
+        // checks if row matches
         if (checkedRow[0].className==checkedRow[1].className 
         && checkedRow[1].className==checkedRow[2].className){
-            //checks for a win in each row
+            // sets the winning slots to the winner class
             for (var j = 0; j<3; j++) {
-                //This if makes sure the space isn't empty
+                // makes sure the space isn't empty
                     if (checkedRow[j].className != 'blank'){
-                        checkedRow[j].className += ' winner'
+                        checkedRow[j].className = 'winner'
                     }
                     
-                }
+                }// end of j
             }
-    }
+    }//end of i
 }
 
-
+// checks for a win in each col
 function colCheckForWin() {
     for (var y=0; y<3; y++){
         //makes an array for the current col
@@ -70,12 +72,12 @@ function colCheckForWin() {
         for (var x=0; x<3; x++){
             checkedCol.push(board[x][y])
         }
-        //checks for win in each col
+        //checks for win for the col in the current array
         if (checkedCol[0].className==checkedCol[1].className
         && checkedCol[1].className==checkedCol[2].className){
             for (var i=0; i<3; i++){
                 if (checkedCol[i].className != 'blank'){
-                    checkedCol[i].className += ' winner'
+                    checkedCol[i].className = 'winner'
                 }
             }
             //makes sure space isn't empty
