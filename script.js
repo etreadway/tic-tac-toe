64bfd5b3a672d37d11afb471959a7f874cba6322
Eric Treadway <eric.s.treadway@gmail.com>
2021-03-05 08:28:09 -0600

    added checks for win condition on diagnal lines

--- script.js
+++ script.js
@@ -1,48 +1,63 @@
 var boardElement = document.getElementById('board');
 var board = [];
 var turn = 'x';
-// iterates through every row
-for (var row = 0; row < 3; row++) {
-    if (board[row] == undefined) {
-        board[row] = [];
-    }
 
-    // iterates through every col 
-    for (var col = 0; col < 3; col++) {
-        // this makes each row appear on a new line
-        if (col == 0){
-            boardElement.appendChild(document.createElement('br'));
+// builds initial board and gives squares functionality
+function initGame() {
+    // iterates through every row
+    for (var row = 0; row < 3; row++) {
+        if (board[row] == undefined) {
+            board[row] = [];
         }
 
-        // creates each slot and gives it a blank image
-        var slot = document.createElement("img");
-        slot.className = "blank";
-        slot.src = 'images/blank.png';
-
-        // gives each slot click functionality
-        slot.addEventListener("click", function () {
-            // if statement makes sure the spot is not taken
-            if (this.className == 'blank'){
-                // checks who's turn it is and assigns proper image
-                if (turn == 'x'){
-                    this.src = 'images/x.png';
-                    this.className = 'x';
-                    turn = 'o';
-                } else {
-                    this.src = 'images/o.png';
-                    this.className = 'o';
-                    turn = 'x';
-                }
-                rowCheckForWin();
-                colCheckForWin();
+        // iterates through every col 
+        for (var col = 0; col < 3; col++) {
+            // this makes each row appear on a new line
+            if (col == 0){
+                boardElement.appendChild(document.createElement('br'));
             }
-            
-        });
-        board[row][col] = slot;
-        boardElement.appendChild(slot);
-    }//end of col
-    
-}//end of row
+
+            // creates each slot and gives it a blank image
+            var slot = document.createElement("img");
+            slot.className = "blank";
+            slot.src = 'images/blank.png';
+
+            // gives each slot click functionality
+            slot.addEventListener("click", function () {
+                // if statement makes sure the spot is not taken
+                if (this.className == 'blank'){
+                    // checks who's turn it is and assigns proper image
+                    if (turn == 'x'){
+                        this.src = 'images/x.png';
+                        this.className = 'x';
+                        turn = 'o';
+                    } else {
+                        this.src = 'images/o.png';
+                        this.className = 'o';
+                        turn = 'x';
+                    }
+                    // rowCheckForWin();
+                    // colCheckForWin();
+                    boardStateCheck()
+                }
+                
+            });
+            // array that you can change
+            board[row][col] = slot;
+            // appends to the html document
+            boardElement.appendChild(slot);
+        }//end of col
+        
+    }//end of row
+
+}
+
+//runs all board checks
+function boardStateCheck() {
+    rowCheckForWin();
+    colCheckForWin();
+    diagCheckForWin();
+}
 
 // checks for a win on each row
 function rowCheckForWin() {
@@ -86,4 +101,29 @@ function colCheckForWin() {
 
     }  
 }
+
+// checks for a win on the diagonals
+function diagCheckForWin(){
+    var diag1 = [board[0][0],board[1][1],board[2][2]]
+    var diag2 = [board[0][2],board[1][1],board[2][0]]
+    var bothDiags = [diag1,diag2]
+
+    for (var x=0; x<2; x++){
+        // checks for win on current diag
+        if (bothDiags[x][0].className==bothDiags[x][1].className
+            && bothDiags[x][1].className==bothDiags[x][2].className){
+                // makes sure the spots aren't blank then makes them winners
+                for (var i=0; i<3; i++){
+                    if (bothDiags[x][i].className != 'blank'){
+                        bothDiags[x][i].className = 'winner'
+                    }
+                }//end of i
+            }
+    }//end of x
+
     
+    
+
+}
+    
+initGame()
